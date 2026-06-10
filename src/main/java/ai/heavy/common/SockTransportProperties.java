5e1be8f0dc771d8dd4263848b9a38bd168f06594
misiugodfrey <misiug@nvidia.com>
2026-06-10 16:24:15 -0700

    chore: add NVIDIA SPDX headers to 14 source files
    Directly inserts NVIDIA SPDX license headers into files that did not
    receive them in the previous commit (verify-copyright only updates files
    that already have some form of copyright header). Part of org-wide SPDX
    header rollout (see heavydb-internal#8287).

--- src/main/java/ai/heavy/common/SockTransportProperties.java
+++ src/main/java/ai/heavy/common/SockTransportProperties.java
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: Copyright (c) 2026, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 package com.mapd.common;
 
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
